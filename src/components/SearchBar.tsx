bd68c249e90b2943ba298875c956102296680b64
andrewmoquia <andrewmoquia09@gmail.com>
2023-02-22 15:06:10 +0800

    feat: styling search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,40 @@
-import { View, Text } from 'react-native';
+import { View, StyleSheet, TextInput } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
     return (
-        <View>
-            <Text>Search Bar</Text>
+        <View style={styles.searchBar}>
+            <View style={styles.searchIcon}>
+                <Feather name='search' size={24} color='black' />
+            </View>
+            <TextInput style={styles.searchText} placeholder='Search hotel...' />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    searchBar: {
+        alignItems: 'center',
+        backgroundColor: '#e9eaea',
+        borderRadius: 10,
+        flexDirection: 'row',
+        height: 50,
+        margin: 10,
+    },
+    searchIcon: {
+        alignItems: 'center',
+        // backgroundColor: 'grey',
+        height: 50,
+        justifyContent: 'center',
+        width: 50,
+    },
+    searchText: {
+        alignSelf: 'center',
+        // borderWidth: 1,
+        flex: 1,
+        fontSize: 20,
+        height: 50,
+    },
+});
 
 export default SearchBar;
--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,8 +1,10 @@
 import { View, Text } from 'react-native';
+import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
     return (
         <View>
+            <SearchBar />
             <Text>Search Screen!</Text>
         </View>
     );
