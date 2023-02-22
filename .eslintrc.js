fc460ae643d2aa6d2d011d8bdc7b18294969192a
andrewmoquia <john.andrew@singlife.com>
2023-02-22 12:32:27 +0800

    fix: eslint and typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,45 @@
 module.exports = {
     env: {
-        "react-native/react-native": true
+        'react-native/react-native': true,
     },
-    plugins: [
-        "react",
-        "react-native",
-        '@typescript-eslint'
-    ],
+    plugins: ['react', 'react-native', '@typescript-eslint', 'eslint-plugin-prefer-arrow'],
     extends: [
         'eslint:recommended',
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react-native/all",
-        "plugin:prettier/recommended",
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-native/all',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+        'plugin:prettier/recommended',
+        'plugin:eslint-comments/recommended',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
+        // project: 'tsconfig.json',
         ecmaFeatures: {
-            jsx: true
+            jsx: true,
         },
         ecmaVersion: 12,
-        sourceType: "module",
+        sourceType: 'module',
     },
     rules: {
         'prettier/prettier': ['error', { endOfLine: 'auto' }],
-        "react-native/no-unused-styles": 2,
-        "react-native/split-platform-components": 2,
-        "react-native/no-inline-styles": 2,
-        "react-native/no-color-literals": 2,
-        "react-native/no-raw-text": 2,
-        "react-native/no-single-element-style-arrays": 2,
-    }
-}
+        'react-native/no-unused-styles': 2,
+        'react-native/split-platform-components': 2,
+        'react-native/no-inline-styles': 2,
+        'react-native/no-color-literals': 0,
+        'react-native/no-raw-text': 2,
+        'react-native/no-single-element-style-arrays': 2,
+        '@typescript-eslint/no-unused-vars': ['error'],
+        'prefer-arrow/prefer-arrow-functions': 'error',
+        '@typescript-eslint/naming-convention': [
+            'error',
+            {
+                selector: 'variable',
+                format: ['camelCase', 'PascalCase'],
+            },
+        ],
+        '@typescript-eslint/no-namespace': 'error',
+        'import/namespace': 'off',
+    },
+};
